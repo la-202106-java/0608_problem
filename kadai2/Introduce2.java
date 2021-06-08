b7278865724d4be1a65c592548545dded0d70340
hogehoge153 <swampman25@via.tokyo.jp>
2021-06-08 17:32:45 +0900

    re kadai2

--- kadai2/Introduce2.java
+++ kadai2/Introduce2.java
@@ -1,5 +1,20 @@
 package kadai2;
 
-public class Introduce2 {
+import java.util.Scanner;
 
+public class Introduce2 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("名前を入力してください:");
+		String name = scan.nextLine();
+		System.out.print("年齢を入力してください:");
+		String age = scan.nextLine();
+		int num = Integer.parseInt(age);
+		if (name.length() < 0 || 130 < name.length()) {
+			System.out.println("名前は20文字以内で入力してください");
+		}
+
+		System.out.print(name + "さんの10年後は" + (num + 10) + "歳です");
+	}
 }

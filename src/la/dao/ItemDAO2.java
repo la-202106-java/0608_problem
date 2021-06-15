329ea8862346f710bbf6f0d0ea4ce50791aeb113
hogehoge153 <swampman25@via.tokyo.jp>
2021-06-15 17:10:24 +0900

    kadai5_STEP2

---
+++ src/la/dao/ItemDAO2.java
@@ -0,0 +1,244 @@
+package la.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import la.bean.ItemBean;
+
+public class ItemDAO2 {
+	private Connection con;
+
+	public ItemDAO2() throws DAOException {
+		getConnection();
+	}
+
+	public List<ItemBean> findAll() throws DAOException {
+		if (con == null)
+			getConnection();
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			// SQL文の作成
+			String sql = "SELECT * FROM item";
+			// PreparedStatementオブジェクトの取得
+			st = con.prepareStatement(sql);
+			// SQLの実行
+			rs = st.executeQuery();
+			// 結果の取得
+			List<ItemBean> list = new ArrayList<ItemBean>();
+			while (rs.next()) {
+				int code = rs.getInt("code");
+				String name = rs.getString("name");
+				int price = rs.getInt("price");
+				ItemBean bean = new ItemBean(code, name, price);
+				list.add(bean);
+			}
+			// 商品一覧をListとして返す
+			return list;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new DAOException("レコードの取得に失敗しました。");
+		} finally {
+			try {
+				// リソースの開放
+				if (rs != null)
+					rs.close();
+				if (st != null)
+					st.close();
+				close();
+			} catch (Exception e) {
+				throw new DAOException("リソースの開放に失敗しました。");
+			}
+		}
+	}
+
+	public List<ItemBean> sortPrice(boolean isAscending) throws DAOException {
+		if (con == null)
+			getConnection();
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			// SQL文の作成
+			String sql;
+			// ソートキーの指定
+			if (isAscending)
+				sql = "SELECT * FROM item ORDER BY price";
+			else
+				sql = "SELECT * FROM item ORDER BY price desc";
+			// PreparedStatementオブジェクトの取得
+			st = con.prepareStatement(sql);
+			// SQLの実行
+			rs = st.executeQuery();
+			// 結果の取得および表示
+			List<ItemBean> list = new ArrayList<ItemBean>();
+			while (rs.next()) {
+				int code = rs.getInt("code");
+				String name = rs.getString("name");
+				int price = rs.getInt("price");
+				ItemBean bean = new ItemBean(code, name, price);
+				list.add(bean);
+			}
+			// 商品一覧をListとして返す
+			return list;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new DAOException("レコードの操作に失敗しました。");
+		} finally {
+			try {
+				// リソースの開放
+				if (rs != null)
+					rs.close();
+				if (st != null)
+					st.close();
+				close();
+			} catch (Exception e) {
+				throw new DAOException("リソースの開放に失敗しました。");
+			}
+		}
+	}
+
+	public int addItem(String name, int price) throws DAOException {
+		if (con == null)
+			getConnection();
+
+		PreparedStatement st = null;
+		try {
+			// SQL文の作成
+			String sql = "INSERT INTO item(name, price) VALUES(?, ?)";
+			// PreparedStatementオブジェクトの取得
+			st = con.prepareStatement(sql);
+			// ソートキーの指定
+			st.setString(1, name);
+			st.setInt(2, price);
+			// SQLの実行
+			int rows = st.executeUpdate();
+			return rows;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new DAOException("レコードの操作に失敗しました。");
+		} finally {
+			try {
+				// リソースの開放
+				if (st != null)
+					st.close();
+				close();
+			} catch (Exception e) {
+				throw new DAOException("リソースの開放に失敗しました。");
+			}
+		}
+	}
+
+	public List<ItemBean> findByPrice(int minPrice, int maxPrice) throws DAOException {
+		if (con == null)
+			getConnection();
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			// SQL文の作成
+			String sql = "SELECT * FROM item WHERE price >= ? AND price <= ?";
+			// PreparedStatementオブジェクトの取得
+			st = con.prepareStatement(sql);
+			if (minPrice == 0) {
+				// 値段のセット
+				st.setInt(1, minPrice);
+				st.setInt(2, maxPrice);
+			} else if (maxPrice == 100000000) {
+				// 値段のセット
+				st.setInt(1, minPrice);
+				st.setInt(2, maxPrice);
+			} else {
+				// 値段のセット
+				st.setInt(1, minPrice);
+				st.setInt(2, maxPrice);
+			}
+			// SQLの実行
+			rs = st.executeQuery();
+			// 結果の取得および表示
+			List<ItemBean> list = new ArrayList<ItemBean>();
+			while (rs.next()) {
+				int code = rs.getInt("code");
+				String name = rs.getString("name");
+				int price = rs.getInt("price");
+				ItemBean bean = new ItemBean(code, name, price);
+				list.add(bean);
+			}
+			// 商品一覧をListとして返す
+			return list;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new DAOException("レコードの操作に失敗しました。");
+		} finally {
+			try {
+				// リソースの開放
+				if (rs != null)
+					rs.close();
+				if (st != null)
+					st.close();
+				close();
+			} catch (Exception e) {
+				throw new DAOException("リソースの開放に失敗しました。");
+			}
+		}
+	}
+
+	public int deleteByPrimaryKey(int key) throws DAOException {
+		if (con == null)
+			getConnection();
+
+		PreparedStatement st = null;
+		try {
+			// SQL文の作成
+			String sql = "DELETE FROM item WHERE code = ?";
+			// PreparedStatementオブジェクトの取得
+			st = con.prepareStatement(sql);
+			// 主キーの指定
+			st.setInt(1, key);
+			// SQLの実行
+			int rows = st.executeUpdate();
+			return rows;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new DAOException("レコードの操作に失敗しました。");
+		} finally {
+			try {
+				// リソースの開放
+				if (st != null)
+					st.close();
+				close();
+			} catch (Exception e) {
+				throw new DAOException("リソースの開放に失敗しました。");
+			}
+		}
+	}
+
+	private void getConnection() throws DAOException {
+		try {
+			// JDBCドライバの登録
+			Class.forName("org.postgresql.Driver");
+			// URL、ユーザ名、パスワードの設定
+			String url = "jdbc:postgresql:sample";
+			String user = "student";
+			String pass = "himitu";
+			// データベースへの接続
+			con = DriverManager.getConnection(url, user, pass);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new DAOException("接続に失敗しました。");
+		}
+	}
+
+	private void close() throws SQLException {
+		if (con != null) {
+			con.close();
+			con = null;
+		}
+	}
+}
---
+++ src/la/servlet/ItemServlet2.java
@@ -0,0 +1,112 @@
+package la.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import la.bean.ItemBean;
+import la.dao.DAOException;
+import la.dao.ItemDAO2;
+
+@WebServlet("/ItemServlet2")
+public class ItemServlet2 extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		try {
+			request.setCharacterEncoding("UTF-8");
+			// パラメータの解析
+			String action = request.getParameter("action");
+			// モデルのDAOを生成
+			ItemDAO2 dao = new ItemDAO2();
+			// パラメータなしの場合は全レコード表示
+			if (action == null || action.length() == 0) {
+				List<ItemBean> list = dao.findAll();
+				// Listをリクエストスコープに入れてJSPへフォーワードする
+				request.setAttribute("items", list);
+				gotoPage(request, response, "/showItem2.jsp");
+				// addは追加
+			} else if (action.equals("add")) {
+				String name = request.getParameter("name");
+				int price = Integer.parseInt(request.getParameter("price"));
+				dao.addItem(name, price);
+				// 追加後、全レコード表示
+				List<ItemBean> list = dao.findAll();
+				// Listをリクエストスコープに入れてJSPへフォーワードする
+				request.setAttribute("items", list);
+				gotoPage(request, response, "/showItem2.jsp");
+			}
+			// sortはソート
+			else if (action.equals("sort")) {
+				String key = request.getParameter("key");
+				List<ItemBean> list;
+				if (key.equals("price_asc")) {
+					list = dao.sortPrice(true);
+				} else {
+					list = dao.sortPrice(false);
+				}
+				// Listをリクエストスコープに入れてJSPへフォーワードする
+				request.setAttribute("items", list);
+				gotoPage(request, response, "/showItem2.jsp");
+			}
+			// searchは検索
+			else if (action.equals("search")) {
+				if (request.getParameter("minprice").equals("")) {
+					int minPrice = 0;
+					int maxPrice = Integer.parseInt(request.getParameter("maxprice"));
+					List<ItemBean> list = dao.findByPrice(minPrice, maxPrice);
+					request.setAttribute("items", list);
+					gotoPage(request, response, "/showItem2.jsp");
+				} else if (request.getParameter("maxprice").equals("")) {
+					int maxPrice = 100000000;
+					int minPrice = Integer.parseInt(request.getParameter("minprice"));
+					List<ItemBean> list = dao.findByPrice(minPrice, maxPrice);
+					request.setAttribute("items", list);
+					gotoPage(request, response, "/showItem2.jsp");
+				} else {
+					int minPrice = Integer.parseInt(request.getParameter("minprice"));
+					int maxPrice = Integer.parseInt(request.getParameter("maxprice"));
+					List<ItemBean> list = dao.findByPrice(minPrice, maxPrice);
+					request.setAttribute("items", list);
+					gotoPage(request, response, "/showItem2.jsp");
+				}
+
+			}
+			// deleteは削除
+			else if (action.equals("delete")) {
+				int code = Integer.parseInt(request.getParameter("code"));
+				dao.deleteByPrimaryKey(code);
+				// 削除後、全レコード表示
+				List<ItemBean> list = dao.findAll();
+				// Listをリクエストスコープに入れてJSPへフォーワードする
+				request.setAttribute("items", list);
+				gotoPage(request, response, "/showItem2.jsp");
+			} else {
+				request.setAttribute("message", "正しく操作してください。");
+				gotoPage(request, response, "/errInternal.jsp");
+			}
+		} catch (DAOException e) {
+			e.printStackTrace();
+			request.setAttribute("message", "内部エラーが発生しました。");
+			gotoPage(request, response, "/errInternal.jsp");
+		}
+	}
+
+	private void gotoPage(HttpServletRequest request,
+			HttpServletResponse response, String page) throws ServletException,
+			IOException {
+		RequestDispatcher rd = request.getRequestDispatcher(page);
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}

b671f2853dfbc7451f2f9b7118a5ab0fa4fc049b
st5166 <ds09027475166@gmail.com>
2021-06-22 11:45:53 +0900

    Add adminLogin

---
+++ WebContent/adminTop.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>トップメニュー(管理者)</title>
+</head>
+<body>
+上手くいった URLはこちら
+</body>
+</html>
--- src/la/dao/AdminDAO.java
+++ src/la/dao/AdminDAO.java
@@ -1,5 +1,65 @@
 package la.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class AdminDAO {
+	private Connection con;
+
+	public AdminDAO() throws DAOException {
+		getConnection();
+	}
+
+	public boolean isAdmin(String email, String password) throws DAOException {
+		if (con == null) {
+			getConnection();
+		}
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		String sql = "SELECT * FROM admin WHERE email_address=? AND pass=?";
+		try {
+			st = con.prepareStatement(sql);
+			st.setString(1, email);
+			st.setString(2, password);
+			rs = st.executeQuery();
+			return (rs.getString("email_address") != null);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new DAOException("レコードの取得に失敗しました");
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (st != null)
+					st.close();
+				close();
+			} catch (Exception e) {
+				throw new DAOException("リソースの開放に失敗しました");
+			}
+		}
+	}
+
+	private void getConnection() throws DAOException {
+		try {
+			Class.forName("org.postgresql.Driver");
+			String url = "jdbc:postgresql:reservationsystem";
+			String user = "student";
+			String pass = "himitu";
+			con = DriverManager.getConnection(url, user, pass);
+		} catch (Exception e) {
+			throw new DAOException("接続に失敗しました");
+		}
+
+	}
+
+	private void close() throws SQLException {
+		if (con != null) {
+			con.close();
+			con = null;
+		}
+	}
 
 }

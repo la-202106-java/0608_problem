5660610109a079f9bf27908b20dde524a91e55c3
la-yukasato <floor.satoh+la@gmail.com>
2021-06-25 10:10:03 +0900

    add by sato 20210625

--- src/la/servlet/Shiryou_haikiServlet.java
+++ src/la/servlet/Shiryou_haikiServlet.java
@@ -1,6 +1,8 @@
 package la.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,8 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import la.bean.Material;
 import la.bean.MaterialLedger;
 import la.dao.DAOException;
+import la.dao.MaterialDAO;
 import la.dao.MaterialLedgerDAO;
 
 @WebServlet("/Shiryou_haikiServlet")
@@ -23,20 +27,26 @@ public class Shiryou_haikiServlet extends HttpServlet {
 
 			String action = request.getParameter("action");
 
-			MaterialLedgerDAO dao = new MaterialLedgerDAO();
 			if (action.equals("dispose_confirm")) {//廃棄確認
+				MaterialDAO dao = new MaterialDAO();
 				int id = Integer.parseInt(request.getParameter("material_id"));
-				String isbn = (request.getParameter("isbn"));
-				String stockDate = request.getParameter("stockDate");
-				String title = request.getParameter("title");
+				List<Material> list = new ArrayList<Material>();
+				list = dao.findById(id);//資料idに紐づくレコードの取得
 
-				request.setAttribute("material_id", id);
-				request.setAttribute("isbn", isbn);
-				request.setAttribute("stockDate", stockDate);
-				request.setAttribute("title", title);
+				//				String isbn = (request.getParameter("isbn"));
+				//				String stockDate = request.getParameter("stockDate");
+				//				String title = request.getParameter("title");
+
+				//				request.setAttribute("material_id", id);
+				//				request.setAttribute("isbn", isbn);
+				//				request.setAttribute("stockDate", stockDate);
+				//				request.setAttribute("title", title);
+				request.setAttribute("items", list);
 				gotoPage(request, response, "/confirm_shiryou_haiki.jsp");
 
 			} else if (action.equals("dispose_ok")) {//廃棄OK
+				MaterialLedgerDAO dao = new MaterialLedgerDAO();
+
 				int id = Integer.parseInt(request.getParameter("material_id"));
 				@SuppressWarnings("unused")
 				int rows = dao.disposeById(id);//廃棄年月日の更新

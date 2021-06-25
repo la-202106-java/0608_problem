a777a91156ccc8f1b836fec3bf2fd9446ae96016
taichi tsuru <5197793.ds@gmail.com>
2021-06-25 16:41:29 +0900

    1641

--- src/black/servlet/DeleteServlet.java
+++ src/black/servlet/DeleteServlet.java
@@ -30,48 +30,50 @@ public class DeleteServlet extends HttpServlet {
 			ListedItemDAO dao = new ListedItemDAO();
 			//商品情報の取得
 			HttpSession session = request.getSession(false);
+			//String user = (String) session.getAttribute("user");
 
-			if ((session.getAttribute("user").equals("member")) || (session.getAttribute("user").equals("admin"))) {
+			//			if ((user.equals("member")) || (user.equals("admin"))) {
+			//				String seller_id = request.getParameter("seller_id");
+			//				if (seller_id.equals("id")) {
 
-				if (request.getAttribute("seller_id").equals("id"))
-
-					//教科書削除チェック選択画面
-					if (action.equals("delete")) {
-						ListedItemBean item = (ListedItemBean) session.getAttribute("item");
-						String isbn = item.getIsbn();
-						request.setAttribute("setIsbn", isbn);
-						String title = item.getAuthor();
-						request.setAttribute("setTitle", title);
-						int department_code = item.getDepartmentCode();
-						request.setAttribute("SetDepartment_code", department_code);
-						String author = item.getAuthor();
-						request.setAttribute("setAuthor", author);
-						int price = item.getPrice();
-						request.setAttribute("SetPrice", price);
-						gotoPage(request, response, "/listedItemDeleteCheck.jsp");
-					} else if (action.equals("dodelete")) {
-						//削除完了ページに行く
-						ListedItemBean item = (ListedItemBean) session.getAttribute("item");
-						int id = item.getId();
-						dao.deleteItem(id);
-						session.removeAttribute("item");
-						gotoPage(request, response, "/listedItemDeleteDone.jsp");
-					}
-					//キャンセル　教科書詳細ページに行く
-					else if (action.equals("cancel")) {
-						gotoPage(request, response, "/ListedItemDetail.jsp");
-					}
-
-					//教科書削除完了後、トップページに戻る
-					else if (action.equals("topReturn")) {
-						gotoPage(request, response, "/top");
-					}
-
+			//教科書削除チェック選択画面
+			if (action.equals("delete")) {
+				int id = Integer.parseInt(request.getParameter("item_id"));
+				ListedItemBean item = dao.findItem(id);
+				String isbn = item.getIsbn();
+				request.setAttribute("setIsbn", isbn);
+				String title = item.getAuthor();
+				request.setAttribute("setTitle", title);
+				int department_code = item.getDepartmentCode();
+				request.setAttribute("SetDepartment_code", department_code);
+				String author = item.getAuthor();
+				request.setAttribute("setAuthor", author);
+				int price = item.getPrice();
+				request.setAttribute("SetPrice", price);
+				session.setAttribute("item", item);
+				gotoPage(request, response, "/listedItemDeleteCheck.jsp");
+			} else if (action.equals("dodelete")) {
+				//削除完了ページに行く
+				ListedItemBean item = (ListedItemBean) session.getAttribute("item");
+				int id = item.getId();
+				dao.deleteItem(id);
+				session.removeAttribute("item");
+				gotoPage(request, response, "/listedItemDeleteDone.jsp");
+			}
+			//キャンセル　教科書詳細ページに行く
+			else if (action.equals("cancel")) {
+				gotoPage(request, response, "/ListedItemDetail.jsp");
 			}
 
-		} catch (
+			//教科書削除完了後、トップページに戻る
+			else if (action.equals("topReturn")) {
+				gotoPage(request, response, "/top");
+			}
 
-		Exception e) {
+			//				}
+			//			}
+
+		} catch (Exception e) {
 			e.printStackTrace();
 			request.setAttribute("message", "内部エラーが発生しました");
 			gotoPage(request, response, "/errInternal.jsp");
